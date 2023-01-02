384a141fe25d58af75fb66eb9c685263240e29ba
Paul Curry <pcurry@gmail.com>
2023-01-02 15:46:23 -0500

    Create schema.py
    Sketch of a DB schema to store relevant information. Written in SQLAlchemy Declarative Base mostly because I miss using SQLAlchemy and as an excuse to write something in SQLAlchemy 2.0 for the heck of it.

---
+++ model_training/schema.py
@@ -0,0 +1,62 @@
+import sqlalchemy as sa
+
+from sqlalchemy.orm import DeclarativeBase
+
+# declarative base class
+class Base(DeclarativeBase):
+    pass
+
+
+class RailRoad(Base):
+    pass
+
+
+class Manufacturer(Base):
+    pass
+
+
+class Gauge(Base):
+    pass
+
+
+class Product(Base):
+    # Has a Manufacturer
+    # Has an Item_Number
+    # (Manufacturer, Item_Number) should be unique but I'm not sure yet
+    # Might have a Gauge
+    
+    pass
+    
+class TrainSet(Base):
+    # Is a product
+    # Has zero to many of
+    #  - engines
+    #  - cars
+    #  - track sections
+    #  - power supplies
+    #  - controllers
+    pass
+
+class TrackSection(Base):
+    # has a gauge
+    # is a type of track
+    pass
+
+
+class TrackInventory(Base):
+    # Has a tracksection ID, foreign key to TrackSection
+    # has a count of how many of those sections are currently present
+    pass
+
+
+class Layout(Base):
+    """The thing with all the track where the model trains actually roll around.
+    """
+
+
+class LayoutInventory(Base):
+    # possibly how we indicate which items are part of which layout?
+    
+    
+    
+                 
