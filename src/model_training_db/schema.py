40390d70dbe062bc683df54b95a19e3916111905
Paul Curry <pcurry@gmail.com>
2023-10-07 23:56:59 -0400

    Stab at declaring the datastructure of the SQL DB

--- src/model_training_db/schema.py
+++ src/model_training_db/schema.py
@@ -1,6 +1,30 @@
+from typing import List
+from typing import Optional
+
 import sqlalchemy as sa
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
+
+# class User(Base):
+#     __tablename__ = "user_account"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(30))
+#     fullname: Mapped[Optional[str]]
+#     addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+#     def __repr__(self) -> str:
+#         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+# class Address(Base):
+#     __tablename__ = "address"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     email_address: Mapped[str]
+#     user_id = mapped_column(ForeignKey("user_account.id"))
+#     user: Mapped[User] = relationship(back_populates="addresses")
+#     def __repr__(self) -> str:
+#         return f"Address(id={self.id!r}, email_address={self.email_address!r})"
 
 # declarative base class
 class Base(DeclarativeBase):
@@ -10,25 +34,43 @@ class Base(DeclarativeBase):
 class Railroad(Base):
     __tablename__ = "railroad"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    abbreviation: Mapped[str]
+
 
 
 class Manufacturer(Base):
     __tablename__ = "manufacturer"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
 
-class Gauge(Base):
-    __tablename__ = "track_gauge"
+    products: Mapped[List["Product"]] = relationship(back_populates="manufacturer")
+
+
+class Scale(Base):
+    __tablename__ = "scale"
+    name: Mapped[str] = mapped_column(String(2), primary_key=True)
 
 
 class Product(Base):
+    __tablename__ = "product"
+
+    id: Mapped[int] = mapped_column(primay_key=True)
+    manufacturer_id = mapped_column(ForeignKey("manufacturer.id"))
+    manufacturer: Mapped[Manufacturer] = relationship(back_populates="products")
+
     # Has a Manufacturer
     # Has an Item_Number
     # (Manufacturer, Item_Number) should be unique but I'm not sure yet
+    #
     # Might have a Gauge
 
-    pass
 
 class TrainSet(Base):
+    __tablename__ = "trainset"
+
     # Is a product
     # Has zero to many of
     #  - engines
@@ -36,9 +78,10 @@ class TrainSet(Base):
     #  - track sections
     #  - power supplies
     #  - controllers
-    pass
+
 
 class TrackSection(Base):
+
     # has a gauge
     # is a type of track
     pass
